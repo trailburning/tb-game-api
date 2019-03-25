95aa089768f471062be5378e6b54960adbb06606
Matt Allbeury <mallbeury@mac.com>
2019-03-25 12:34:46 +0100

    Change file name

--- upload.php
+++ upload.php
@@ -21,11 +21,11 @@ $s3 = new Aws\S3\S3Client([
     ]
 ]);
 
-if($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_FILES['userfile']) && $_FILES['userfile']['error'] == UPLOAD_ERR_OK && is_uploaded_file($_FILES['userfile']['tmp_name'])) {
+if($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_FILES['upload_file']) && $_FILES['upload_file']['error'] == UPLOAD_ERR_OK && is_uploaded_file($_FILES['upload_file']['tmp_name'])) {
 
   try {
     // FIXME: do not use 'name' for upload (that's the original filename from the user's computer)
-    $upload = $s3->upload($bucket, $_FILES['userfile']['name'], fopen($_FILES['userfile']['tmp_name'], 'rb'), 'public-read');
+    $upload = $s3->upload($bucket, $_FILES['upload_file']['name'], fopen($_FILES['upload_file']['tmp_name'], 'rb'), 'public-read');
 
     echo htmlspecialchars($upload->get('ObjectURL'));
 
