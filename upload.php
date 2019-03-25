518cb9533c2e92d590276a19259c89314ad1da64
Matt Allbeury <mallbeury@mac.com>
2019-03-25 11:57:22 +0100

    CORS

--- upload.php
+++ upload.php
@@ -1,6 +1,7 @@
 <?php
-error_reporting(E_ALL);
-ini_set('display_errors', 1);
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Methods: GET, POST');
+header("Access-Control-Allow-Headers: X-Requested-With");
 
 require 'vendor/autoload.php';
 
