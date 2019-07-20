1e64e3ac1a80b9b6e536bef136e28366fc772439
Matt Allbeury <mallbeury@mac.com>
2019-07-20 22:02:04 +0200

    test

--- index.php
+++ index.php
@@ -97,6 +97,84 @@ $app->get('/worker', function (Request $request, Response $response) {
   }
 });
 
+$app->get('/eventbrite', function (Request $request, Response $response) {
+
+  $provider = new \League\OAuth2\Client\Provider\GenericProvider([
+      'clientId'                => 'RONZPMWFZISMTR6FKW',    // The client ID assigned to you by the provider
+      'clientSecret'            => 'EWRRVO67U6ADB2WSNPASAV7LQXYSVMMVE5GFT6MAAPWPJQ44RB',   // The client password assigned to you by the provider
+      'redirectUri'             => 'https://tb-game-api-staging.herokuapp.com/eventbrite',
+      'urlAuthorize'            => 'https://www.eventbrite.com/oauth/authorize',
+      'urlAccessToken'          => 'https://www.eventbrite.com/oauth/token',
+      'urlResourceOwnerDetails' => 'https://www.eventbrite.com/oauth/resource'
+  ]);
+
+  // If we don't have an authorization code then get one
+  if (!isset($_GET['code'])) {
+
+      // Fetch the authorization URL from the provider; this returns the
+      // urlAuthorize option and generates and applies any necessary parameters
+      // (e.g. state).
+      $authorizationUrl = $provider->getAuthorizationUrl();
+
+      // Get the state generated for you and store it to the session.
+      $_SESSION['oauth2state'] = $provider->getState();
+
+      // Redirect the user to the authorization URL.
+      header('Location: ' . $authorizationUrl);
+      exit;
+
+  // Check given state against previously stored one to mitigate CSRF attack
+  } elseif (empty($_GET['state']) || (isset($_SESSION['oauth2state']) && $_GET['state'] !== $_SESSION['oauth2state'])) {
+
+      if (isset($_SESSION['oauth2state'])) {
+          unset($_SESSION['oauth2state']);
+      }
+      
+      exit('Invalid state');
+
+  } else {
+
+      try {
+
+          // Try to get an access token using the authorization code grant.
+          $accessToken = $provider->getAccessToken('authorization_code', [
+              'code' => $_GET['code']
+          ]);
+
+          // We have an access token, which we may use in authenticated
+          // requests against the service provider's API.
+          echo 'Access Token: ' . $accessToken->getToken() . "<br>";
+          echo 'Refresh Token: ' . $accessToken->getRefreshToken() . "<br>";
+          echo 'Expired in: ' . $accessToken->getExpires() . "<br>";
+          echo 'Already expired? ' . ($accessToken->hasExpired() ? 'expired' : 'not expired') . "<br>";
+
+          // Using the access token, we may look up details about the
+          // resource owner.
+          $resourceOwner = $provider->getResourceOwner($accessToken);
+
+          var_export($resourceOwner->toArray());
+
+          // The provider provides a way to get an authenticated API request for
+          // the service, using the access token; it returns an object conforming
+          // to Psr\Http\Message\RequestInterface.
+          $request = $provider->getAuthenticatedRequest(
+              'GET',
+              'http://brentertainment.com/oauth2/lockdin/resource',
+              $accessToken
+          );
+
+      } catch (\League\OAuth2\Client\Provider\Exception\IdentityProviderException $e) {
+
+          // Failed to get the access token or user details.
+          exit($e->getMessage());
+
+      }
+
+  }
+
+
+});
+
 $app->get('/strava/subscribe', function (Request $request, Response $response) {
   $url = 'https://api.strava.com/api/v3/push_subscriptions';
 
