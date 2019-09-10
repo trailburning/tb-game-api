a4d7efec5a65396f11b1a4899304d18eff906182
Matt Allbeury <mallbeury@mac.com>
2019-09-10 11:22:20 +0200

    test

--- lib/tbPlayer.php
+++ lib/tbPlayer.php
@@ -296,6 +296,8 @@ function updatePlayer($playerID) {
     $client = new Client($service);
     $activities = $client->getAthlete();
 
+    echo 'name:' . $activities['firstname'];
+
     updatePlayerDetailsWithoutEmailInDB($activities['profile'], $activities['firstname'], $activities['lastname'], $activities['city'], $activities['country'], $token);
   }
 }
